8e4e31d760de1ee2110312f866d7827bac9c3ceb
widesky0 <kingyan09@qq.com>
2019-09-22 22:25:01 +0800

    '8.事件处理器'

---
+++ src/pages/logs/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+      <swiper v-if="imgUrls.length > 0" indidator-dots="imgUrls.length > 1" >
+      <block v-for="(item, index) in imgUrls" :key="index" >
+        <swiper-item>
+          <image :src="item" mode="scaleToFill"></image>
+        </swiper-item>
+      </block>
+    </swiper>
+
+    <ul class="container log-list">
+      <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
+        <card :text="(index + 1) + ' . ' + log"></card>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { formatTime } from '@/utils/index'
+import card from '@/components/card'
+
+export default {
+  components: {
+    card
+  },
+
+     created() {
+    console.log('page logs created', this)
+  },
+  mounted() {
+    console.log('mounted', this)
+  },
+  onLoad() {
+    console.log('page logs onLoad', this)
+  },
+  onReady () {
+    console.log('page logs onReady', this)
+  },
+  onShow() {
+    console.log('onShow', this)
+  },
+  onUnload() {
+    console.log('onUnload', this)
+  },
+  onHide() {
+    console.log('onHide', this)
+  },
+
+  data () {
+    return {
+      logs: [],
+      imgUrls: [
+        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/newsPicture/05558951-de60-49fb-b674-dd906c8897a6',
+        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/coursePicture/0fbcfdf7-0040-4692-8f84-78bb21f3395d',
+        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/management-school-picture/7683b32e-4e44-4b2f-9c03-c21f34320870'
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+.log-list {
+  display: flex;
+  flex-direction: column;
+  padding: 40rpx;
+}
+
+.log-item {
+  margin: 10rpx;
+}
+</style>
---
+++ src/pages/use/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+   <div>自定义表单组件</div>
+    <myInput fromLabTitle="用户年龄" formInputPh="请输入用户年龄" @getInputVal="inputVal">
+    </myInput>
+     <myInput fromLabTitle="用户姓名" formInputPh="请输入用户姓名" >
+     </myInput>
+  </div>
+</template>
+
+<script>
+import myInput from "@/components/myinput.vue"
+
+export default {
+    data(){
+      return{
+      title:"username",
+      value:"tom"
+      }
+     
+    },
+    methods:{
+      inputVal(e){
+        console.log(e)
+      }
+    },
+    components:{
+      myInput
+    }
+   
+}
+</script>
+
+<style>
+
+</style>
