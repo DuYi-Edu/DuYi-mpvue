73dc32f8489d32b6eb4515251bad2a3a797518ef
widesky0 <kingyan09@qq.com>
2019-09-22 22:07:56 +0800

    'mpvue快速入门'

---
+++ src/pages/test/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+        <template v-if="loginType === 'username'">
+    <label>Username</label>
+    <input placeholder="Enter your username" key="username-input">
+    </template>
+    <template v-else>
+    <label>Email</label>
+    <input placeholder="Enter your email address" key="email-input">
+    </template>
+    <button @click="aa">aaa</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            loginType:"username"
+        };
+    },
+
+    methods:{
+        aa(){
+            this.loginType="aaa"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
