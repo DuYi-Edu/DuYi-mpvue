d326fe0d463e728c7a6142a8377b3f440fca8cf0
widesky0 <kingyan09@qq.com>
2019-09-22 22:15:01 +0800

    '5.计算属性'

---
+++ src/pages/test/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <div>{{sayHello}}</div>
+      <div>{{reversedSayHello}}</div>
+      <div>{{showString}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            sayHello:"hello"
+        }
+    },
+
+    methods:{
+        
+    },
+
+    computed:{
+        //多逻辑操作的属性（普通计算属性）
+        reversedSayHello(){
+            return this.sayHello.split('').reverse().join('')
+        },
+        //缓存
+       showString(){
+           return "showString"
+       }
+
+    }
+} 
+
+
+
+
+</script>
+
+<style>
+
+</style>
