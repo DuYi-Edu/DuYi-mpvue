8ea86fd69908220145d01672fee366d15a3c0d42
widesky0 <kingyan09@qq.com>
2019-09-22 22:30:35 +0800

    '9.表单控件绑定'

---
+++ src/components/myinput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <label class="labelTag">{{fromLabTitle}}</label>
+      <input type="text" v-model="inputval" placeholder-class="font-red" :placeholder="formInputPh" @change="getInputVal"/>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['fromLabTitle','formInputPh'],
+    data(){
+        return{
+            inputval:''
+        }
+    },
+    methods:{
+        getInputVal(){
+            //触发
+            this.$emit("getInputVal",this.inputval)
+        }
+    }
+}
+</script>
+
+<style>
+input{
+    color: green;
+}
+
+.font-red{
+    color:red;
+}
+
+</style>
---
+++ src/pages/event/index.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+    区别window对象操作和小程序事件的写法
+    <button @click="add(3)">Add</button>
+    <p>The button above has been clicked {{ counter }} times.</p>
+
+    事件修饰符在mpvue的使用
+    .stop
+    <div @click="outerEvent">
+        outer
+        <div @click.stop="innerEvent">inner</div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            counter:0
+        };
+    },
+
+    methods:{
+        outerEvent(){
+            console.log("outerEventMethods")
+        },
+        innerEvent(){
+            console.log("innerEventMethods")
+        },
+        add(num){
+            //this.counter +=num
+            //在mpvue中是不能直接使用window对象的任何方法
+            //alert(num)
+            //原生微信写法提示框
+            wx.showToast({
+                title:'成功',
+                icon:"success",
+                duration:2000
+            })
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
